4fb3f5ec0fcf682c6f734adbe70b6453698b3c61
Martin <voyager.martin@gmail.com>
2026-07-30 21:01:36 +0800

    feat: upgrade to v2.5.3 with stepped dynamic take-profit QA guide

--- 程式碼.js
+++ 程式碼.js
@@ -1,7 +1,7 @@
 /**
  * Market Engine V3 - 整合型 Google Sheet 自動建置與維護腳本
  * Single Source of Truth 架構：市場觀察 + MARKET LAB 合一
- * Version: v2.5.2 (大師心態與邏輯解密 QA 觀念導航升級版)
+ * Version: v2.5.3 (階梯式動態停利指南 QA 閉環升級版)
  */
 
 /**
@@ -195,7 +195,7 @@ function setupMarketEngineV3() {
   ss.setActiveSheet(dashboardSheet);
   ss.moveActiveSheet(1);
 
-  SpreadsheetApp.getUi().alert('🎉 Market Engine V3 (v2.5.2) 更新完成！\n已成功導入觀念導航大師心態與邏輯解密 QA 區塊！');
+  SpreadsheetApp.getUi().alert('🎉 Market Engine V3 (v2.5.3) 更新完成！\n已成功導入階梯式動態停利指南 QA 閉環升級版！');
 }
 
 /**
